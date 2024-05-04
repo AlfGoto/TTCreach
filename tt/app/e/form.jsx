02dcef886d409a6d48420c3e9c966eb226b11503
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-05-04 22:41:44 +0200

    component bug is fixed

--- tt/app/e/form.jsx
+++ tt/app/e/form.jsx
@@ -15,7 +15,7 @@ import { EditorGender } from '/components/editorGender'
 import { EditorCity } from '/components/editorCity'
 import { EditorBio } from '/components/editorBio'
 import { EditorHappy } from '/components/editorHappy'
-import { EditorDesc } from '/components/EditorDescr'
+import { EditorDesc } from '/components/editorDescr'
 
 
 function FormElement() {
